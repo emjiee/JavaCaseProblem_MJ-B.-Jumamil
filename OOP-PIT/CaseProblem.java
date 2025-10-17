1301ea66d4974275d5772374b895587e9c3b6dc3
emjiee <jumamilmj2@gmail.com>
2025-10-17 12:11:42 +0800

    Uploaded Java case problem and output screenshot.

---
+++ OOP-PIT/CaseProblem.java
@@ -0,0 +1,14 @@
+public class CaseProblem {
+    public static void main(String[] args) {
+        
+        System.out.println("Name: MJ Jumamil");
+        System.out.println("ID Number: 2024303259");
+        System.out.println("----------------------------------");
+
+        int num1 = 20;
+        int num2 = 30;
+        int sum = num1 + num2;
+
+        System.out.println("Result: The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
